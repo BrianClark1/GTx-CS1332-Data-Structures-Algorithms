1338d67e1264f69c531662423fb1cf1c0e0d8fd1
Brian Clark <briansclark1@gmail.com>
2023-05-29 21:48:43 -0400

    Linked List Boilerplate code

--- LinkedList.java
+++ LinkedList.java
@@ -1,24 +1,105 @@
-public class LinkedList {
+import java.util.NoSuchElementException;
 
-    public void recrusiveDuplicate(node) {
-//Recursion Duplicate solution 
-//We are going to need to traverse the entire linked list 
-        
-        //Base Case, end of iteration
-        if (node.next == null) {
-            return;
-        }
+/**
+ * Your implementation of a Singly-Linked List.
+ */
+public class SinglyLinkedList<T> {
 
-        //We have a duplicate
-    else if(node.next.value==node.value){
-            //Point our current nodes next to skip the duplicate, keeping list integrity
-            node.next = node.next.next;
-            //Point our duplicated next to null, effectivley removing it from the sequence. (nothing point to it, it points to nothing)
-            node.next.next = null;
-        }
-        return recrusiveDuplicate(node.next);
+    /*
+     * Do not add new instance variables or modify existing ones.
+     */
+    private SinglyLinkedListNode<T> head;
+    private SinglyLinkedListNode<T> tail;
+    private int size;
 
+    /*
+     * Do not add a constructor.
+     */
+
+    /**
+     * Adds the element to the front of the list.
+     *
+     * Method should run in O(1) time.
+     *
+     * @param data the data to add to the front of the list
+     * @throws java.lang.IllegalArgumentException if data is null
+     */
+    public void addToFront(T data) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    }
+
+    /**
+     * Adds the element to the back of the list.
+     *
+     * Method should run in O(1) time.
+     *
+     * @param data the data to add to the back of the list
+     * @throws java.lang.IllegalArgumentException if data is null
+     */
+    public void addToBack(T data) {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    }
+
+    /**
+     * Removes and returns the first data of the list.
+     *
+     * Method should run in O(1) time.
+     *
+     * @return the data formerly located at the front of the list
+     * @throws java.util.NoSuchElementException if the list is empty
+     */
+    public T removeFromFront() {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    }
+
+    /**
+     * Removes and returns the last data of the list.
+     *
+     * Method should run in O(n) time.
+     *
+     * @return the data formerly located at the back of the list
+     * @throws java.util.NoSuchElementException if the list is empty
+     */
+    public T removeFromBack() {
+        // WRITE YOUR CODE HERE (DO NOT MODIFY METHOD HEADER)!
+    }
+
+    /**
+     * Returns the head node of the list.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the node at the head of the list
+     */
+    public SinglyLinkedListNode<T> getHead() {
+        // DO NOT MODIFY THIS METHOD!
+        return head;
+    }
+
+    /**
+     * Returns the tail node of the list.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the node at the tail of the list
+     */
+    public SinglyLinkedListNode<T> getTail() {
+        // DO NOT MODIFY THIS METHOD!
+        return tail;
+    }
+
+    /**
+     * Returns the size of the list.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the size of the list
+     */
+    public int size() {
+        // DO NOT MODIFY THIS METHOD!
+        return size;
+    }
 }
-
-
-
